5fd6cc7b39cbbabe680156b164a0283f69624477
Ahsan Bashir <mahsanbashir104@gmail.com>
2024-04-22 17:06:03 +0500

    dashboard updated

--- app/page.jsx
+++ app/page.jsx
@@ -11,34 +11,33 @@ export default function Home() {
     <div>
           <div className='border w-screen h-screen'> {/* bg screen */}
               <div className='border  w-11/12 h-full m-auto flex fex-row'>
-                <div className='  border-r-2 bg-slate-700 w-3/12 h-full bg-gradient-to-b from-slate-600  to-blue-300 '>
-                <div className=' rounded-full w-4/12 h-26 m-auto mt-4 bg-blue-900 p-2 border-2 border-yellow-500 '>
-                     {/* <image src={Logo} height={80} width={100}/> */}
-                     {/* <Image src={Logo} className=''/> */}
-                  </div>
-                 
-                  <div className=' w-full m-auto'>
-                      <ul>
-                     
-                            <li className='pl-5 bg-slate-200  mb-1 mt-2 hover:bg-white cursor-pointer  border-indigo-500'>
-                              Add Product
-                            </li>
-                            <li className='pl-5 bg-slate-200  mb-1 hover:bg-white cursor-pointer  border-indigo-500'>
-                              <Link href="">View Products</Link>
-                              </li>
-                            
-                      </ul>
-                  </div>
-                </div> 
+                
                 {/* Header Area */}
-                 <div className='bg-gradient-to-b from-slate-200  to-gray-100 w-9/12 h-full'>
-                    <div className=' justify-center items-center  to-slate-100 w-full  flex flex-row mb-1 h-1/6'>
-                   
+                 <div className='w-9/12 h-full'>
+                    
+                    {/* Total Previous Recrods Tab */}
+                    <div className='  justify-center items-center   w-full  flex flex-row mb-1 h-1/6'>
+                        <div className=" text-white flex justify-center items-center align-middle bg-gradient-to-tr from-blue-900 to-blue-300 p-2 flex-col  m-2  w-3/12 rounded-lg shadow-md shadow-black">
+                          <h1>Total Pass Out Trainees </h1>
+                          <h5>5000</h5>
+                          
+                          
+                          </div>
+                        <div className="bg-gradient-to-tr from-blue-800 to-blue-300 p-6 m-2 w-3/12  rounded-lg">dsfd</div>
+                        <div className="bg-gradient-to-tr from-blue-800 to-blue-300 p-6 m-2 w-3/12  rounded-lg">dsfd</div>
+
                     </div> {/*end of Header Area*/}
                     
+                    <div className=' border border-black justify-center items-center  bg-slate-900 w-full  flex flex-row mb-1 h-8 text-white'>
+                        <h1>Present Undergoing Courses</h1>
+
+                    </div>
+
                     {/* Container Area */}
-                    <div className='bg-white w-full h-full flex flex-row'>
-                        <div className='bg-blue-400 w-3/12 h-3/6 flex flex-row border-black m-1 '>
+                    <div className=' w-full h-full  border border-black bg-blue-200 flex'>
+                      
+
+                        <div className='bg-blue-400 w-3/12 h-3/6 flex flex-row border-black mt-1 '>
                           <div className=' w-full bg-slate-200'>
                              <h6 className='bg-orange-600 p-1 text-white text-center'>Advance Class Course</h6>
                              <ul>
@@ -81,13 +80,13 @@ export default function Home() {
                         <div className='bg-blue-400 w-3/12 h-3/6 flex flex-row border-black m-1 '>
                           <div className=' w-full bg-slate-200'>
                              <h6 className='bg-black p-1 text-white text-center'>Recruite Course</h6>
-                             {/* <ul>
+                             <ul>
                                 <li className='pl-1 border-slate-100 border-b-2 mb-1 hover:font-bold hover:bg-slate-200  hover:rounded-md cursor-pointer'>Total Nominatation: 30</li>
                                 <li className='pl-1 border-slate-100 border-b-2 mb-1 hover:font-bold hover:bg-slate-200  hover:rounded-md cursor-pointer'>Total Arrival: 27</li>
                                 <li className='pl-1 border-slate-100 border-b-2 mb-1 hover:font-bold hover:bg-slate-200  hover:rounded-md cursor-pointer'>Not Reported: 03</li>
                                 <li className='pl-1 border-slate-100 border-b-2 mb-1 hover:font-bold hover:bg-slate-200  hover:rounded-md cursor-pointer'>Repatriated: 02</li>
                                 <li className='pl-1 border-slate-100 border-b-2 mb-1 hover:font-bold hover:bg-slate-200  hover:rounded-md cursor-pointer'>Present: 25</li>
-                             </ul> */}
+                             </ul>
                              
                             </div>
                         </div>
