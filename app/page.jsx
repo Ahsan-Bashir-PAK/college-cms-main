ffeeeeba18c39a98cc21ec8174aa83faf1472f6b
Ahsan Bashir <mahsanbashir104@gmail.com>
2024-05-07 17:21:18 +0500

    account verf updated

--- app/page.jsx
+++ app/page.jsx
@@ -2,39 +2,84 @@
 import axios from "axios"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
   
 
 export default function Home() {
 
+   useEffect(()=>{
+       
+      axios.get('http://localhost:5000/reports/get').then(
+              response=>{
+                      if (response) { console.log("sdddddddddd" + response.data)
+                              setData(response.data) 
+                      
+                      }
+                       else { alert("not working") }
+              }
+
+      ).catch((error)=>{
+              console.log(error)
+      })
+},[])
+
+const [data, setData] = useState([])
+
   return (
     <div>
-          <div className='border w-screen h-screen'> {/* bg screen */}
-              <div className='border  w-11/12 h-full m-auto flex fex-row'>
+          <div className='border w-full h-screen'> {/* bg screen */}
+              <div className='border  w-full h-full m-auto flex flex-row  p-2'>
                 
                 {/* Header Area */}
-                 <div className='w-9/12 h-full'>
+                 <div className='w-full h-full'>
                     
                     {/* Total Previous Recrods Tab */}
-                    <div className='  justify-center items-center   w-full  flex flex-row mb-1 h-1/6'>
-                        <div className=" text-white flex justify-center items-center align-middle bg-gradient-to-tr from-blue-900 to-blue-300 p-2 flex-col  m-2  w-3/12 rounded-lg shadow-md shadow-black">
-                          <h1>Total Pass Out Trainees </h1>
-                          <h5>5000</h5>
-                          
-                          
+                    <div className='  justify-center items-center   w-full  flex flex-row mb-2  '>
+                        <div className="  flex justify-center items-center  text-black p-2 flex-col  m-2  w-4/12 rounded-lg shadow-md shadow-black cursor-pointer hover:bg-slate-200 hover:shadow-none">
+                          <h1 className="text-blue-950">Total Pass Out Trainees </h1>
+                          <h5 className="font-bold">4678</h5>
                           </div>
-                        <div className="bg-gradient-to-tr from-blue-800 to-blue-300 p-6 m-2 w-3/12  rounded-lg">dsfd</div>
-                        <div className="bg-gradient-to-tr from-blue-800 to-blue-300 p-6 m-2 w-3/12  rounded-lg">dsfd</div>
+
+                          <div className="  bg-[#fccd41] flex justify-center items-center align-middle  text-black p-2 flex-col  m-2  w-2/12 rounded-lg shadow-md shadow-black cursor-pointer  hover:shadow-none ">
+                          <h1> Advance  </h1>
+                          <h5 className="font-bold">1580</h5>
+                          </div>
+
+                          <div className="bg-[#ff000c] text-white flex justify-center items-center align-middle  p-2 flex-col  m-2  w-2/12 rounded-lg shadow-md shadow-black cursor-pointer  hover:shadow-none">
+                          <h1>Upper  </h1>
+                          <h5 className="font-bold">5000</h5>
+                          </div>
+
+                          <div className="  flex justify-center items-center align-middle  text-black p-2 flex-col  m-2  w-2/12 rounded-lg shadow-md shadow-black cursor-pointer  hover:shadow-none">
+                          <h1>Lower   </h1>
+                          <h5 className="font-bold">5000</h5>
+                          </div>
+                          <div className="  flex justify-center items-center align-middle   p-2 flex-col  m-2  w-2/12 rounded-lg shadow-md shadow-black cursor-pointer  hover:shadow-none bg-yellow-400 text-black ">
+                          <h1>Orientation   </h1>
+                          <h5 className="font-bold">5000</h5>
+                          </div>
+                          <div className="  flex justify-center items-center align-middle  text-white p-2 flex-col  m-2  w-2/12 rounded-lg shadow-md shadow-black cursor-pointer bg-green-700 hover:shadow-none  ">
+                          <h1>Probationer   </h1>
+                          <h5 className="font-bold">5000</h5>
+                          </div>
+
+                          <div className="  flex justify-center items-center align-middle  text-white p-2 flex-col  m-2  w-2/12 rounded-lg shadow-md shadow-black cursor-pointer  hover:shadow-none bg-black ">
+                          <h1>Recruit   </h1>
+                          <h5 className="font-bold">5000</h5>
+                          </div>
+
+                          
 
                     </div> {/*end of Header Area*/}
                     
-                    <div className=' border border-black justify-center items-center  bg-slate-900 w-full  flex flex-row mb-1 h-8 text-white'>
+                    <div className=' rounded-md justify-center items-center  bg-slate-300 w-full  flex flex-row mb-1 h-8 text-black'>
                         <h1>Present Undergoing Courses</h1>
 
                     </div>
 
                     {/* Container Area */}
-                    <div className=' w-full h-full  border border-black bg-blue-200 flex'>
+                    <div className=' w-full h-full  border  bg-blue-200 p-6 flex'>
                       
 
                         <div className='bg-blue-400 w-3/12 h-3/6 flex flex-row border-black mt-1 '>
--- app/trainee/accountVerf/page.jsx
+++ app/trainee/accountVerf/page.jsx
@@ -9,7 +9,7 @@ const AccountVerf = ()=>{
 
         useEffect(()=>{
        
-                axios.get('http://localhost:5000/register/getRegAccounts').then(
+                axios.get('http://116.0.45.14:5000/signUp/accountReq').then(
                         response=>{
                                 if (response) { console.log(response.data)
                                         setData(response.data) 
@@ -29,54 +29,49 @@ const [data, setData] = useState([])
 
 
 return (
-        <div className='bg-slate-200  border w-10/12 h-4/5 m-auto p-2 items-center'>
-               <div className="flex flex-row justify-center w-full p-4 gap-2 bg-gray-300">
-                
-                <h1 className=' text-black  rounded-md text-lg text-center font-bold'> List of Trainee Registered Through Android Application
-                </h1>
-               </div>
-                
-                <div className='flex flex-row w-full '>
-                        <div className='flex flex-col   border-blue-900 w-full  rounded-tl-md'>
-                        <div className='flex flex-row  text-white bg-blue-400  '>
-                        <div className='p-1 w-1/12 '>Sr. #</div>
-                        <div className='p-1 ml-2 w-3/12 '>Trainee Name</div>
-                        <div className='p-1 ml-2 w-2/12 '>Father Name</div>
-                        <div className='p-1 ml-2 w-1/12 '>CNIC.</div>
-                        <div className='p-1 ml-2 w-1/12 '>Blood Group.</div>
-                        <div className='p-1 mr-2 w-1/12  '>Date of Birth</div>
-                        <div className='p-1 ml-2 w-1/12 '>Qualification</div>
-                        <div className='p-1 ml-2 w-1/12 '>Domicile</div>
-                        <div className='p-1 mr-2 w-1/12  '>Personal Contact</div>
-                        <div className='p-1 mr-2 w-1/12  '>Emergency Contact</div>
-                        <div className='p-1 mr-2 w-1/12  '>Emergency Contact (Relation)</div>
-                        
-                    </div>        
+                <div className='bg-slate-200  border w-10/12 h-4/5 m-auto p-2 items-center'>
+                        <div className="flex flex-row justify-center w-full p-4 gap-2 bg-gray-300">
+
+                                <h1 className=' text-black  rounded-md text-lg text-center font-bold'> List of Trainee Registered Through Android Application
+                                </h1>
                         </div>
-                        
-                        
+
+                        <div className='flex flex-row w-full '>
+                                <table className='w-full justify-center items-center text-center'>
+                                        <tr className=' bg-blue-950 p-2 text-white '>
+                                                <td className='w-2/12 '>Trainee Name</td>
+                                                <td className='w-2/12 '>Father Name</td>
+                                                <td className='w-3/12 '>Course</td>
+                                                <td className='w-2/12 '>CNIC #</td>
+                                                <td className='w-2/12 '>Domicile</td>
+                                                <td className='w-1/12 '>Action Taken</td>
+                                        </tr>
+
+
+                                </table>
+
+
+
+                        </div>
+                        <div>
+                                {data &&
+                                        data.map((item, index) => (
+                                                
+                        <tr className='p-1 border-b border-blue-950 flex '>
+                                <td className='w-2/12 border '>{item.tName} </td>
+                                <td className='w-2/12  '>{item.tFName}</td>
+                                <td className='w-3/12 '>{item.tCourse}</td>
+                                <td className='w-2/12 '>{item.tCnic}</td>
+                                <td className='w-2/12 '>{item.tDomicile}</td>
+                                <td className='w-1/12 items-center  justify-center gap-5 flex flex-row'><FaCheckCircle color='green' /><MdEdit /></td>
+                                </tr>
+                    
+                                        ))}
+
+                        </div>
+
+
                 </div>
-                <div>
-                                {data && 
-                                data.map((item, index)=>(
-                                        <div className='p-2 w-full border-b border-blue-400  flex flex-row'>
-                                        <div className='p-1 ml-2 w-1/12 '>{item.id}</div>
-                                        <div className='p-1 ml-2 w-3/12 '>{item.tName}</div>
-                                        <div className='p-1 ml-2 w-2s/12 '>{item.tFName}</div>
-                                        <div className='p-1 ml-2 w-2/12 '>{item.tCnic}</div>
-                                        <div className='p-1 ml-2 w-2/12 '>{item.tDob}</div>
-                                        <div className='p-1 ml-2 w-1/12 '>{item.tDomicile}</div>
-                                        <div className='p-1 ml-2 w-1/12 '>
-                                                <FaCheckCircle color='green'/> 
-                                                <MdEdit/>
-                                        </div>
-                                        </div>
-                                       
-                                ))}
-                                   
-                        </div>
-                
-                
-        </div>
-)}
+        )
+}
 export default AccountVerf
